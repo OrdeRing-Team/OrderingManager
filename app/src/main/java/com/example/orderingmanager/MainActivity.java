a9646138c293db4353e53dbb305424514ed4aa55
seunggyu97 <seunggyu97@gmail.com>
2022-01-06 21:02:49 +0900

    push test

--- app/src/main/java/com/example/orderingmanager/MainActivity.java
+++ app/src/main/java/com/example/orderingmanager/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //push 테스트
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
