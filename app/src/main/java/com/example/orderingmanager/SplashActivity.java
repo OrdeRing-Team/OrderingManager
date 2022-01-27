dfa2fc6bb209602b32eeed5de38e606325f1b69a
minju park <mlnjv016@gmail.com>
2022-01-27 22:12:44 +0900

    Splash 수정

--- app/src/main/java/com/example/orderingmanager/SplashActivity.java
+++ app/src/main/java/com/example/orderingmanager/SplashActivity.java
@@ -2,22 +2,50 @@ package com.example.orderingmanager;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        try{
-            Thread.sleep(2000);
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        startSplash2();
+        startLoading();
 
-        Intent intent=new Intent(this,MainActivity.class);
-
-        startActivity(intent);
-
-        finish();
     }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();   //현재 액티비티 종료
+            }
+        }, 3000);
+
+    }
+
+    private void startSplash2() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ImageView splash1 = findViewById(R.id.splash1img);
+                splash1.setVisibility(View.GONE);
+                ImageView splash2 = findViewById(R.id.splash2img);
+                splash2.setVisibility(View.VISIBLE);
+            }
+        }, 2000);
+
+    }
+
 }
