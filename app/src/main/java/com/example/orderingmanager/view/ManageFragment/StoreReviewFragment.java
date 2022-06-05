16fadf503bd9eecb2893bee507ddc23736c54642
seunggyu <seunggyu97@gmail.com>
2022-06-06 02:27:38 +0900

    1. [StoreManageActivity] 리뷰 적용 안되는 문제 해결

--- app/src/main/java/com/example/orderingmanager/view/ManageFragment/StoreReviewFragment.java
+++ app/src/main/java/com/example/orderingmanager/view/ManageFragment/StoreReviewFragment.java
@@ -1,5 +1,13 @@
 package com.example.orderingmanager.view.ManageFragment;
 
+import static com.example.orderingmanager.view.ManageFragment.StoreManageActivity.fiveStars;
+import static com.example.orderingmanager.view.ManageFragment.StoreManageActivity.fourStars;
+import static com.example.orderingmanager.view.ManageFragment.StoreManageActivity.oneStar;
+import static com.example.orderingmanager.view.ManageFragment.StoreManageActivity.reviewList;
+import static com.example.orderingmanager.view.ManageFragment.StoreManageActivity.reviewTotalRating;
+import static com.example.orderingmanager.view.ManageFragment.StoreManageActivity.threeStars;
+import static com.example.orderingmanager.view.ManageFragment.StoreManageActivity.twoStars;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -57,104 +65,38 @@ public class StoreReviewFragment extends Fragment {
         initReviewRecyclerView();
     }
 
-    private void initReviewRecyclerView(){
-        try{
-            new Thread(){
-                @SneakyThrows
-                public void run(){
-                    Retrofit retrofit = new Retrofit.Builder()
-                            .baseUrl("http://www.ordering.ml/api/restaurant/" + restaurantId + "/reviews/")
-                            .addConverterFactory(GsonConverterFactory.create())
-                            .build();
+    private void initReviewRecyclerView() {
+        int totalStars = StoreManageActivity.totalStars;
+        binding.progressbar1.setMax(totalStars);
+        binding.progressbar2.setMax(totalStars);
+        binding.progressbar3.setMax(totalStars);
+        binding.progressbar4.setMax(totalStars);
+        binding.progressbar5.setMax(totalStars);
 
-                    RetrofitService service = retrofit.create(RetrofitService.class);
-                    Call<ResultDto<List<ReviewPreviewDto>>> call = service.getReviewList(restaurantId);
+        Log.e("oneStar",Integer.toString(oneStar));
+        Log.e("oneStar",Integer.toString(twoStars));
+        Log.e("oneStar",Integer.toString(threeStars));
+        Log.e("oneStar",Integer.toString(fourStars));
+        Log.e("oneStar",Integer.toString(fiveStars));
+        binding.progressbar1.setProgress(oneStar);
+        binding.progressbar2.setProgress(twoStars);
+        binding.progressbar3.setProgress(threeStars);
+        binding.progressbar4.setProgress(fourStars);
+        binding.progressbar5.setProgress(fiveStars);
 
-                    call.enqueue(new Callback<ResultDto<List<ReviewPreviewDto>>>() {
-                        @Override
-                        public void onResponse(Call<ResultDto<List<ReviewPreviewDto>>> call, Response<ResultDto<List<ReviewPreviewDto>>> response) {
 
-                            if (response.isSuccessful()) {
-                                ResultDto<List<ReviewPreviewDto>> result;
-                                result = response.body();
-                                if (!result.getData().isEmpty()) {
-                                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            float reviewTotalRating = 0;
-                                            int fiveStars = 0, fourStars = 0, threeStars = 0, twoStars = 0, oneStar = 0;
-                                            for(ReviewPreviewDto i : result.getData()){
-                                                reviewTotalRating += i.getRating();
-                                                switch ((int)i.getRating()){
-                                                    case 1:
-                                                        oneStar++;
-                                                        break;
-                                                    case 2:
-                                                        twoStars++;
-                                                        break;
-                                                    case 3:
-                                                        threeStars++;
-                                                        break;
-                                                    case 4:
-                                                        fourStars++;
-                                                        break;
-                                                    case 5:
-                                                        fiveStars++;
-                                                        break;
-                                                    default:
-                                                        break;
-                                                }
-                                            }
+        binding.ratingBar.setRating(reviewTotalRating);
+        binding.tvRating.setText(Float.toString(reviewTotalRating));
 
-                                            int totalStars = oneStar + twoStars + threeStars + fourStars + fiveStars;
-                                            Log.e("totalStars###############",Integer.toString(totalStars));
-                                            binding.progressbar1.setMax(totalStars);
-                                            binding.progressbar2.setMax(totalStars);
-                                            binding.progressbar3.setMax(totalStars);
-                                            binding.progressbar4.setMax(totalStars);
-                                            binding.progressbar5.setMax(totalStars);
+        RecyclerView recyclerView = binding.rvReview;
+        ReviewListAdapter reviewListAdapter = new ReviewListAdapter(reviewList, getContext(), binding.clEmptyReview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(reviewListAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), 1));
 
-                                            binding.progressbar1.setProgress(oneStar);
-                                            binding.progressbar2.setProgress(twoStars);
-                                            binding.progressbar3.setProgress(threeStars);
-                                            binding.progressbar4.setProgress(fourStars);
-                                            binding.progressbar5.setProgress(fiveStars);
 
-                                            Log.e("one two three four five",Integer.toString(oneStar) + Integer.toString(twoStars) +
-                                                    Integer.toString(threeStars) + Integer.toString(fourStars) + Integer.toString(fiveStars));
-                                            reviewTotalRating /= result.getData().size();
-
-                                            binding.ratingBar.setRating(reviewTotalRating);
-                                            binding.tvRating.setText(Float.toString(reviewTotalRating));
-
-                                            RecyclerView recyclerView = binding.rvReview;
-                                            ReviewListAdapter reviewListAdapter = new ReviewListAdapter(result.getData(), getContext(), binding.clEmptyReview);
-                                            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-                                            recyclerView.setAdapter(reviewListAdapter);
-                                            recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), 1));
-                                        }
-                                    });
-                                }else{
-                                    // 리뷰가 한 개도 존재하지 않을 때
-                                    binding.rvReview.setVisibility(View.GONE);
-                                    binding.clEmptyReview.setVisibility(View.VISIBLE);
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<ResultDto<List<ReviewPreviewDto>>> call, Throwable t) {
-                            Toast.makeText(getActivity(), "일시적인 오류가 발생하였습니다.", Toast.LENGTH_LONG).show();
-                            Log.e("e = ", t.getMessage());
-                        }
-                    });
-                }
-            }.start();
-
-        } catch (Exception e) {
-            Toast.makeText(getActivity(), "일시적인 오류가 발생하였습니다.", Toast.LENGTH_LONG).show();
-            Log.e("e = ", e.getMessage());
-        }
     }
+
+
 
 }
